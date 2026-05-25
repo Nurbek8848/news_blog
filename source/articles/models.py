80e6f19556a2a74ee7fdf921d87efbb594442029
nurbeksoltoev <nurbeksoltoev@gmail.com>
2026-05-25 20:14:19 +0600

    В модель Article добавлено новое поле

--- source/articles/models.py
+++ source/articles/models.py
@@ -8,6 +8,7 @@ class Article(models.Model):
     author = models.CharField(max_length=100, null=False, blank=False, verbose_name="Автор")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата редактирования")
+    new_field = models.CharField(null=True, blank=True)
 
     def __str__(self):
         return self.title
