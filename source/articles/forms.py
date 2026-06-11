7d09a525fd0a0e7e3e5a1b555909424e45068ce4
nurbeksoltoev <nurbeksoltoev@gmail.com>
2026-06-11 21:11:13 +0600

    Логика создания, редактирования переделана с использованием дженерика FormView. Удалены лишние файлы, импорты.

--- source/articles/forms.py
+++ source/articles/forms.py
@@ -1,8 +1,39 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
+from django.forms.widgets import CheckboxSelectMultiple
+
 from articles.models import Article
 
 
 class ArticleForm(ModelForm):
     class Meta:
         model = Article
-        fields = ["title", "content", "author", "status", "tags"]
+        fields = ["title", "content", "author", "tags"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get("title")
+        content = cleaned_data.get("content")
+
+        if title and content and title == content:
+            raise ValidationError("Заголовок и описание не могут быть похожи")
+        return cleaned_data
+
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+
+        if len(title) < 10:
+            raise ValidationError('Title is too short!')
+
+        return title
+
+
+class ArticleStatusForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ["title", "content", "author", "tags", "status"]
+
+        widgets = {
+            'tags': CheckboxSelectMultiple(),
+        }
--- source/articles/views.py
+++ source/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 
 from articles.forms import ArticleForm
 from articles.models import Article
@@ -16,46 +17,43 @@ class ArticleListView(TemplateView):
 
 
 class ArticleDetailView(TemplateView):
+    template_name = "articles/article_view.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['article'] = get_object_or_404(Article, pk=self.kwargs.get('pk'))
         return context
 
 
-class ArticleCreateView(View):
-    def get(self, request, *args, **kwargs):
-        form = ArticleForm()
-        return render(request, 'articles/article_create.html', {'form': form})
+class ArticleCreateView(FormView):
+    template_name = "articles/article_create.html"
+    form_class = ArticleForm
+    success_url = reverse_lazy("list")
 
-    def post(self, request, *args, **kwargs):
-        form = ArticleForm(request.POST)
-
-        if form.is_valid():
-            article = form.save()
-            article.tags.set(form.cleaned_data['tags'])
-            return redirect("detail", pk=article.pk)
-        return render(request, 'articles/article_create.html', {'form': form})
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
-class ArticleUpdateView(View):
+class ArticleUpdateView(FormView):
+    template_name = "articles/article_update.html"
+    form_class = ArticleForm
+
     def dispatch(self, request, *args, **kwargs):
-        self.article = get_object_or_404(Article, pk=self.kwargs.get('pk'))
+        self.article = self.get_object()
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request, pk, *args, **kwargs):
-        form = ArticleForm(instance=self.article)
-        context = {'form': form}
-        return render(request, 'articles/article_update.html', context)
+    def get_object(self):
+        return get_object_or_404(Article, pk=self.kwargs.get('pk'))
 
-    def post(self, request, *args, **kwargs):
-        form = ArticleForm(request.POST, instance=self.article)
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.article
+        return kwargs
 
-        if form.is_valid():
-            article = form.save()
-            article.tags.set(form.cleaned_data['tags'])
-            article.save()
-            return redirect("detail", pk=article.pk)
-        return render(request, 'articles/article_update.html', {'form': form})
+    def form_valid(self, form):
+        article = form.save()
+        return redirect("detail", pk=article.pk)
 
 class ArticleDeleteView(View):
     def post(self, request, *args, **kwargs):
