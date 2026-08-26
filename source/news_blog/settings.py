b29d4ff1e24175a18c9858565159247bcbf356bb
nurbeksoltoev <nurbeksoltoev@gmail.com>
2026-08-26 17:16:51 +0600

    исправлен IP сервера

--- source/news_blog/settings.py
+++ source/news_blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = [
-    '.ngrok-free.app', '134.209.224.190'
+    '.ngrok-free.app', '165.245.242.151'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
